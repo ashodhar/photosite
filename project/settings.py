c2d29e49da7192f8980f8cd5020bdd293dabb1db
yash <yash9288dodabhangi@gmail.com>
2021-02-17 23:20:49 +0530

    make it better

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'ze-!b=qh#j=tam2u58_v#@r*7krbvo4@715&ofe0m96deapkew' #os.environ.get("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['http://127.0.0.1/','https://sunandaphotographys.herokuapp.com/']
 
@@ -124,5 +124,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "your gmail"
-EMAIL_HOST_PASSWORD = "your password"
+EMAIL_HOST_USER = "sunandaphotography22@gmail.com"
+EMAIL_HOST_PASSWORD = "lokh@nath1234"
